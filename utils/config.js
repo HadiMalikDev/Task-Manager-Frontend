3b39251c9d37ea8f77f83de40f4c7d2dd5e545ea
Muhammad Hadi <hadimalikdev@gmail.com>
2022-07-13 18:25:27 +0500

    Netlify parameters configuration

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 export default function getDomain() {
-  const isProduction = false;
+  const isProduction = true;
   return isProduction
     ? "https://hadi-task-api-node.herokuapp.com"
     : "http://localhost:5000";
